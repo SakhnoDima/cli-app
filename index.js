3715d76946c95257d9c79b8f0ca33e1c9d50f132
SakhnoDima <134371251+SakhnoDima@users.noreply.github.com>
2023-10-12 12:02:53 +0300

    Step 4-6

--- index.js
+++ index.js
@@ -1,30 +1,42 @@
 const contacts = require("./contacts");
+const { program } = require("commander");
+const colors = require("colors/safe");
 
 const invokeAction = async ({ action, id, name, email, phone }) => {
   switch (action) {
     case "list":
       const allContacts = await contacts.listContacts();
-      return console.log(allContacts);
-      break;
+      console.log(colors.green(allContacts));
+      return;
 
     case "get":
       const contact = await contacts.getContactById(id);
-      return console.log(contact);
-      break;
+      console.log(colors.green(contact));
+      return;
 
     case "add":
       const newContact = await contacts.addContact(name, email, phone);
-      return console.log(newContact);
-      break;
+      console.log(colors.yellow(newContact));
+      return;
 
     case "remove":
       const removeContact = await contacts.removeContact(id);
-      return console.log(removeContact);
-      break;
+      console.log(colors.red(removeContact));
+      return;
 
     default:
       console.warn("\x1B[31m Unknown action type!");
   }
 };
 
-invokeAction({ action: "list" });
+program
+  .option("-a, --action,  <type>")
+  .option("-i, --id,  <type>")
+  .option("-n, --name,  <type>")
+  .option("-e, --email,  <type>")
+  .option("-p, --phone,  <type>");
+program.parse();
+
+const option = program.opts();
+
+invokeAction(option);
